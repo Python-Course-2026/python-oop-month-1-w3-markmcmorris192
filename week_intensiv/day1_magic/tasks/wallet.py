b027e5e2f0697dabdf1d1fa0d038e52ab27b7b99
Tsibik Semion <patrickfishy2525@gmail.com>
2026-02-21 15:06:56 +0300

    day1

--- week_intensiv/day1_magic/tasks/wallet.py
+++ week_intensiv/day1_magic/tasks/wallet.py
@@ -1,5 +1,13 @@
+from unicodedata import name
+
+
 class Wallet:
     """ЗАДАЧА: Сложение кошельков через __add__ (новый Wallet) и длина через __len__ (целый баланс)"""
-    def __init__(self, name, balance): self.name, self.balance = name, balance
-    def __add__(self, other): pass
-    def __len__(self): pass
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+    def __add__(self, other):
+        return Wallet("Joint Account", self.balance + other.balance)
+
+    def __len__(self):
+        return int(self.balance)
--- week_intensiv/day2_encapsulation/tasks/secure_account.py
+++ week_intensiv/day2_encapsulation/tasks/secure_account.py
@@ -1,6 +1,9 @@
 class SecureAccount:
     """ЗАДАЧА: Приватный __balance, геттер @property и депозит с проверкой > 0"""
-    def __init__(self, bal): self.__balance = bal
+    def __init__(self, bal):
+        self.__balance = bal
     @property
-    def balance(self): pass
-    def deposit(self, amt): pass
+    def balance(self):
+        return self.__balance
+    def deposit(self, amt):
+        self.__balance += amt
